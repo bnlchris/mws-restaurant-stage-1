aebcf334f4e0901dd570829d50ecd997b689e19a
Christian Schultz <bnlchris@web.de>
2018-10-01 22:13:27 +0200

    Service Worker installed, URLs have been cached

--- js/sw/sw.js
+++ js/sw/sw.js
@@ -1,20 +1,32 @@
+// Install Service Worker
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open('myCache')
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
 // Array for files to be cached
-let filesToCache = [
-	'/',
-	'./restaurant.html',
-    './css/styles.css',
-    './js/main.js',
-    './js/restaurant_info.js',
-    './js/dbhelper.js',
-    './data/restaurants.json',
-    './img/1.jpg',
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-    './img/10.jpg',
+let urlsToCache = [
+    '/',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/js/dbhelper.js',
 ];
