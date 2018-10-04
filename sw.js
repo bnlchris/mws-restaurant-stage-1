002d0145da0e4ae1fb1192f22a1ca4317016dfd5
Christian Schultz <bnlchris@web.de>
2018-10-04 22:11:50 +0200

    Servicer Worker: Check for old Service Workers and delete them

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('myCache')
       .then(function(cache) {
-        console.log('Opened cache');
+        console.log('Caching ...', urlsToCache);
         return cache.addAll(urlsToCache);
       })
   );
@@ -13,22 +13,22 @@ self.addEventListener('install', function(event) {
 // Array for files to be cached
 let urlsToCache = [
     '/',
-    '/restaurant.html',
-    '/css/styles.css',
-    '/data/restaurants.json',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/js/dbhelper.js',
+    'restaurant.html',
+    'css/styles.css',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'js/dbhelper.js',
 ];
 
 // Service Worker should return requests with cache, otherwise fetch data from network
@@ -70,3 +70,21 @@ self.addEventListener('fetch', function(event) {
             })
         )
 })
+
+// Check for old Service Workers and delete them
+self.addEventListener('activate', function(event) {
+
+  const cacheWhitelist = ['myCache'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
